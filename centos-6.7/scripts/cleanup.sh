649b01853b554b110555941594933adeea7b263d
initcron-abhijit <abhijit.chaudhari@initcron.org>
2015-12-18 19:41:16 +0530

    Keeping yum cache

--- centos-6.7/scripts/cleanup.sh
+++ centos-6.7/scripts/cleanup.sh
@@ -1,6 +1,6 @@
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 rm -rf VBoxGuestAdditions.iso
-yum -y clean all
 rm -rf /tmp/rubygems-*
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY	
+yum makecache
